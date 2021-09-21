adfd69f3d3c5a43cff0ada34e937d26a4017fab6
CEIBA\santiago.restrepo <tiago931220@gmail.com>
2021-09-20 21:20:43 -0500

    Se realiza ajsute en el nombre del paquete y se crea la vista login

--- app/src/androidTest/java/app/proyecto/tiendeo/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/app/proyecto/tiendeo/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.projecto_final_mv2;
+package app.proyecto.tiendeo;
 
 import android.content.Context;
 
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.projecto_final_mv2", appContext.getPackageName());
+        assertEquals("com.proyecto.tiendeo", appContext.getPackageName());
     }
 }
---
+++ app/src/main/java/app/proyecto/tiendeo/Login.java
@@ -0,0 +1,16 @@
+package app.proyecto.tiendeo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.projecto_final_mv2.R;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
