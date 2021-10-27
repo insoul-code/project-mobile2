ac765bef079af1c23e897ca0fdf64ca81554a363
John Mario <johnmgc.dev@gmail.com>
2021-10-26 22:38:36 -0500

    cambios create

--- app/src/main/java/app/proyecto/tiendeo/AddProductActivity.java
+++ app/src/main/java/app/proyecto/tiendeo/AddProductActivity.java
@@ -14,24 +14,48 @@ import android.os.Bundle;
 import android.widget.Toast;
 import android.view.View;
 
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
 
+import java.util.HashMap;
+import java.util.Map;
 
+import app.proyecto.tiendeo.Entities.Product;
 import app.proyecto.tiendeo.databinding.ActivityAddProductBinding;
+import app.proyecto.tiendeo.databinding.ActivityEditProductBinding;
 
 
 public class AddProductActivity extends AppCompatActivity {
     private ActivityAddProductBinding addProductBinding;
     private FirebaseStorage storage;
     private StorageReference storageReference;
+    private Product product;
+    private FirebaseFirestore db;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         addProductBinding = ActivityAddProductBinding.inflate(getLayoutInflater());
         View v = addProductBinding.getRoot();
         setContentView(v);
+        addProductBinding.btnProduct.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        Intent intent = getIntent();
+        product = (Product) intent.getSerializableExtra("product");
+        db = FirebaseFirestore.getInstance();
+        addProductBinding.edtProduct.setText(product.getName());
+        addProductBinding.edtDescription.setText(product.getDescription());
+        addProductBinding.edtPrice.setText(String.valueOf(product.getPrice()));
+        addProductBinding.edtStockAvailable.setText(String.valueOf(product.getStock()));
+        addProductBinding.edtCategory.setText(product.getCategory());
+
     }
     public void selectImageFromGallery(View view){
         Intent intent = new Intent(Intent.ACTION_PICK);
@@ -57,4 +81,27 @@ public class AddProductActivity extends AppCompatActivity {
                 }
             }
     );
+
+
+    public void onClick(View v) {
+        Map<String, Object> dataProduct = new HashMap<>();
+        dataProduct.put("name",addProductBinding.edtProduct.getText().toString());
+        dataProduct.put("description",addProductBinding.edtDescription.getText().toString());
+        dataProduct.put("price", Double.parseDouble(addProductBinding.edtPrice.getText().toString()));
+        dataProduct.put("stock", Integer.parseInt(addProductBinding.edtStockAvailable.getText().toString()));
+        dataProduct.put("category",addProductBinding.edtCategory.getText().toString());
+        if (v.getId() == addProductBinding.btnProduct.getId()){
+            db.collection("products")
+                    .add(dataProduct)
+                    .addOnSuccessListener(unused -> {
+                        Toast.makeText(getApplicationContext(), "El producto se actualizo correctamente", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(getApplicationContext(), Home.class);
+                        startActivity(intent);
+                        finish();
+                    })
+                    .addOnFailureListener(e -> {
+                        Toast.makeText(getApplicationContext(), "Error al actualizar el producto", Toast.LENGTH_SHORT).show();
+                    });
+        }
+    }
 }
