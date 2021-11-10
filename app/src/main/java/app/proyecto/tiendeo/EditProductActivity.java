bff0754e9f16b0df15c3467400930d3e9d0323a3
DESKTOP-QO5H97P\foox2 <foox299@gmail.com>
2021-11-10 13:15:21 -0500

    Se agrego la visualizacion de la imagen al editar y la envia a la vista del home

--- app/src/main/java/app/proyecto/tiendeo/EditProductActivity.java
+++ app/src/main/java/app/proyecto/tiendeo/EditProductActivity.java
@@ -8,18 +8,28 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 
 import app.proyecto.tiendeo.Entities.Product;
@@ -33,6 +43,8 @@ public class EditProductActivity extends AppCompatActivity implements View.OnCli
     private Product product;
     private StorageReference storageReference;
     private FirebaseFirestore db;
+    private ProgressDialog progressDialog;
+    Uri imageUri, downloadUri;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,6 +60,11 @@ public class EditProductActivity extends AppCompatActivity implements View.OnCli
         editProductBinding.editPrice.setText(String.valueOf(product.getPrice()));
         editProductBinding.editStockAvailable.setText(String.valueOf(product.getStock()));
         editProductBinding.editCategory.setText(product.getCategory());
+        Glide.with(getApplicationContext())
+                .load(product.getImage())
+                .centerCrop()
+                .fitCenter()
+                .into(editProductBinding.ivProduct);
     }
 
     public void selectImageFromGallery(View view){
@@ -66,10 +83,12 @@ public class EditProductActivity extends AppCompatActivity implements View.OnCli
                         Uri uri = data.getData();
                         if(uri != null){
                             editProductBinding.ivProduct.setImageURI(uri);
+                            imageUri = uri;
                         }
                     }
                     else{
                         Toast.makeText(getApplicationContext(), "canceled",Toast.LENGTH_LONG).show();
+                        imageUri = null;
                     }
                 }
             }
@@ -77,25 +96,92 @@ public class EditProductActivity extends AppCompatActivity implements View.OnCli
 
     @Override
     public void onClick(View v) {
-        Map<String, Object> dataProduct = new HashMap<>();
-        dataProduct.put("name",editProductBinding.editProduct.getText().toString());
-        dataProduct.put("description",editProductBinding.editDescription.getText().toString());
-        dataProduct.put("price", Double.valueOf(editProductBinding.editPrice.getText().toString()));
-        dataProduct.put("stock", Integer.valueOf(editProductBinding.editStockAvailable.getText().toString()));
-        dataProduct.put("category",editProductBinding.editCategory.getText().toString());
-        if (v.getId() == editProductBinding.btnProduct.getId()){
-            db.collection("products")
-                    .document(product.getId())
-                    .update(dataProduct)
-                    .addOnSuccessListener(unused -> {
-                        Toast.makeText(getApplicationContext(), "El producto se actualizo correctamente", Toast.LENGTH_SHORT).show();
-                        Intent intent = new Intent(getApplicationContext(), Home.class);
-                        startActivity(intent);
-                        finish();
-                    })
-                    .addOnFailureListener(e -> {
-                        Toast.makeText(getApplicationContext(), "Error al actualizar el producto", Toast.LENGTH_SHORT).show();
-                    });
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle("Uploading File....");
+        progressDialog.show();
+
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy_MM_dd_HH_mm_ss", Locale.CANADA);
+        Date now = new Date();
+        String fileName = formatter.format(now);
+        storageReference = FirebaseStorage.getInstance().getReference("images/"+fileName);
+        if (imageUri!=null) {
+            UploadTask uploadTask = storageReference.putFile(imageUri);
+
+            Task<Uri> urlTask = uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+                @Override
+                public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                    if (!task.isSuccessful()) {
+                        throw task.getException();
+                    }
+                    return storageReference.getDownloadUrl();
+                }
+            }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+                @Override
+                public void onComplete(@NonNull Task<Uri> task) {
+                    if (task.isSuccessful()) {
+                        Uri downloadUri = task.getResult();
+                        String downloadURL = downloadUri.toString();
+
+                        Map<String, Object> dataProduct = new HashMap<>();
+                        dataProduct.put("name", editProductBinding.editProduct.getText().toString());
+                        dataProduct.put("description", editProductBinding.editDescription.getText().toString());
+                        dataProduct.put("price", Double.parseDouble(editProductBinding.editPrice.getText().toString()));
+                        dataProduct.put("stock", Integer.parseInt(editProductBinding.editStockAvailable.getText().toString()));
+                        dataProduct.put("category", editProductBinding.editCategory.getText().toString());
+                        dataProduct.put("image", downloadURL);
+
+                        if (v.getId() == editProductBinding.btnProduct.getId()) {
+                            db.collection("products")
+                                    .document(product.getId())
+                                    .update(dataProduct)
+                                    .addOnSuccessListener(unused -> {
+                                        Toast.makeText(getApplicationContext(), "El producto se actualizo correctamente", Toast.LENGTH_SHORT).show();
+                                        Intent intent = new Intent(getApplicationContext(), Home.class);
+                                        startActivity(intent);
+                                        finish();
+                                    })
+                                    .addOnFailureListener(e -> {
+                                        Toast.makeText(getApplicationContext(), "Error al actualizar el producto", Toast.LENGTH_SHORT).show();
+                                    });
+                        }
+                        if (progressDialog.isShowing()) {
+                            progressDialog.dismiss();
+                            Intent intent = new Intent(getApplicationContext(), Home.class);
+                            startActivity(intent);
+                            finish();
+                        }
+                    }
+                }
+            });
         }
+        else{
+            Map<String, Object> dataProduct = new HashMap<>();
+            dataProduct.put("name",editProductBinding.editProduct.getText().toString());
+            dataProduct.put("description",editProductBinding.editDescription.getText().toString());
+            dataProduct.put("price", Double.parseDouble(editProductBinding.editPrice.getText().toString()));
+            dataProduct.put("stock", Integer.parseInt(editProductBinding.editStockAvailable.getText().toString()));
+            dataProduct.put("category",editProductBinding.editCategory.getText().toString());
+
+            if (v.getId() == editProductBinding.btnProduct.getId()){
+                db.collection("products")
+                        .document(product.getId())
+                        .update(dataProduct)
+                        .addOnSuccessListener(unused -> {
+                            Toast.makeText(getApplicationContext(), "El producto se actualizo correctamente", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(getApplicationContext(), Home.class);
+                            startActivity(intent);
+                            finish();
+                        })
+                        .addOnFailureListener(e -> {
+                            Toast.makeText(getApplicationContext(), "Error al actualizar el producto", Toast.LENGTH_SHORT).show();
+                        });
+        }
+            if (progressDialog.isShowing()){
+                progressDialog.dismiss();
+                Intent intent = new Intent(getApplicationContext(),Home.class);
+                startActivity(intent);
+                finish();
+            }
     }
+}
 }
